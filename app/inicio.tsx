9e923d7371246e1d2e37134dd3f9b0f0f41ceb97
BRUNA KEMPINSK DOS SANTOS <CG3028259@car.ifsp>
2025-09-22 21:25:01 -0300

    adiciona layout

---
+++ app/inicio.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+function inicio() {
+  return (
+    <View>
+      <Text>akmaksa</Text>
+    </View>
+  );
+}
+
+export default inicio;
