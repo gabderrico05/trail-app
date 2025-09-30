da31379c4c38b93cf3f44f0909b86f990991ff52
Luc4sL4idens <lucaslaidens20@gmail.com>
2025-09-29 21:43:29 -0300

    style: ajustes visuais no TrailCard

--- components/TrailCard.tsx
+++ components/TrailCard.tsx
@@ -1,28 +1,32 @@
 import { Feather, MaterialCommunityIcons } from '@expo/vector-icons';
+import { Image } from 'expo-image';
 import React from "react";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
-const TrailCard = ({ 
-  imagemUrl, 
-  titulo, 
-  tempo, 
-  distancia, 
+const TrailCard = ({
+  imagemUrl = 'https://images.unsplash.com/photo-1448375240586-882707db888b?q=80&w=2070&auto=format&fit=crop',
+  titulo = 'Trilha da Montanha',
+  tempo,
+  distancia,
   nivel,
   onPressDetalhes,
   onPressIniciar,
 }) => {
   return (
-    <View className="min-w-64 max-w-[70%] max-h-min bg-white rounded-2xl overflow-hidden my-2.5 mx-4 shadow-lg pb-1">
-      <Image source={{ uri: imagemUrl }} className="w-full h-40" />
-      
-      <View className="p-4 w-full">
-        <Text className="text-xl font-bold mb-2 text-gray-800">{titulo}</Text>
+    <View className="w-full max-h-min bg-white rounded-2xl overflow-hidden my-2.5 mx-4 shadow-lg p-4">
+      <Image
+        source={{ uri: imagemUrl }}
+        className="w-full h-32 rounded-xl"
+      />
+
+      <View className=" w-full pt-1">
+        <Text className="text-xl font-bold mb-2 text-forestGreen-500">{titulo}</Text>
 
         {/* Seção de Detalhes */}
         <View className="mb-4">
           <View className="flex-row items-center mb-1.5">
             <MaterialCommunityIcons name="clock-time-eight-outline" size={16} color="#c41a1a" />
-            <Text className="ml-2 text-sm text-forestGreen-600">Tempo estimado: aaaaaaaaaaaaaaaaaaaaaaaaaaaa{tempo}</Text>
+            <Text className="ml-2 text-sm text-forestGreen-600">Tempo estimado:{tempo}</Text>
           </View>
           <View className="flex-row items-center mb-1.5">
             <MaterialCommunityIcons name="map-marker-distance" size={16} color="#1a64c4" />
@@ -35,20 +39,20 @@ const TrailCard = ({
         </View>
 
         {/* Botões */}
-        <TouchableOpacity 
+        <TouchableOpacity
           className="bg-[#FDECB9] py-3 rounded-xl items-center mb-2.5"
           onPress={onPressDetalhes}
         >
-          <Text className="text-base font-medium text-gray-700">Ver mais detalhes</Text>
+          <Text className="text- font-medium text-forestGreen-500">Ver mais detalhes</Text>
         </TouchableOpacity>
 
         <TouchableOpacity
           onPress={onPressIniciar}
           activeOpacity={0.7}
-          className="bg-[#387664] flex-row items-center justify-end py-3 rounded-xl pr-3"
+          className="bg-forestGreen-400 flex-row items-center justify-end py-3 rounded-xl pr-3"
         >
-          <Text className="text-white text-lg font-bold mr-1">Iniciar</Text>
-          <Feather name="chevron-right" size={22} color="white" />
+          <Text className="text-white text-md font-bold mr-1">Iniciar</Text>
+          <Feather name="chevron-right" size={16} color="white" />
         </TouchableOpacity>
 
       </View>
