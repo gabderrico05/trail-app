9d1608b6f8563346f15f1071e36298832cb5e335
gaberrico05 <derico.g@aluno.ifsp.edu.br>
2025-10-06 22:24:36 -0300

    feat(SelectTrail): add dynamic avatar on EntityBanner

--- components/EntityBanner.tsx
+++ components/EntityBanner.tsx
@@ -1,9 +1,12 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import { Image, View } from "react-native";
 import ReturnButton from "./ReturnButton";
 import TextFont from "./TextFont";
 
 export default function EntityBanner(parkData: any) {
+
+  const [imageLoaded, setImageLoaded] = useState(!!parkData.image);
+  
   return(
     <View className="w-full bg-lightGray-200 h-min-60 pb-10 px-7">
             
@@ -13,12 +16,33 @@ export default function EntityBanner(parkData: any) {
 
             <View className="w-full items-center justify-center pt-2 ">
               
-              <View className="bg-forestGreen-600 w-40 h-40 rounded-2xl items-center justify-center">
-                 <TextFont className="text-5xl text-white">
-                      PE
-                 </TextFont>
-              </View>
-                 <TextFont className="text-xl font-bold mt-5">
+              { parkData.image && imageLoaded ? 
+              
+                  <View className="bg-white w-40 h-40 rounded-2xl items-center justify-center overflow-hidden">
+                    <Image
+                      source={{ uri: parkData.image }}
+                      className="w-32 h-32"
+                      resizeMode="contain"
+                      onError={() => setImageLoaded(false)}
+                      onLoad={() => setImageLoaded(true) }
+                    />
+                  </View>
+
+                    :
+
+                  <View className="bg-forestGreen-600 w-40 h-40 rounded-2xl items-center justify-center">
+                    <TextFont className="text-5xl text-white">
+                      { (parkData?.name?.charAt(0)?.toUpperCase() ?? '') + (parkData?.name?.split(/\s+/)[1]?.charAt(0)?.toUpperCase() ?? parkData?.name?.charAt(0)?.toUpperCase()) }
+                    </TextFont>
+                  </View>
+          
+              } 
+                 <TextFont
+                  className="text-xl font-bold mt-5"
+                  numberOfLines={1}
+                  adjustsFontSizeToFit={true}
+                  minimumFontScale={0.8}
+                  >
                     {parkData && parkData.name}
                  </TextFont>
             </View>
