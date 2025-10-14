1dd6fe022ba756b0bfe33728884d2fdbd25cd215
gaberrico05 <derico.g@aluno.ifsp.edu.br>
2025-10-13 22:22:19 -0300

    feat(GabaritoFonts): add Gabarito fonts configuration in App

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,25 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import { ActivityIndicator, View } from "react-native";
 
 export default function StackLayout() {
+
+const [fontsLoaded] = useFonts({
+    "Gabarito-Regular": require("../assets/fonts/Gabarito-Regular.ttf"),
+    "Gabarito-Bold": require("../assets/fonts/Gabarito-Bold.ttf"),
+    "Gabarito-ExtraBold": require("../assets/fonts/Gabarito-ExtraBold.ttf"),
+    "Gabarito-Medium": require("../assets/fonts/Gabarito-Medium.ttf"),
+    "Gabarito-SemiBold": require("../assets/fonts/Gabarito-SemiBold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View className="flex-1 items-center justify-center">
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   return (
     <Stack>
       <Stack.Screen
