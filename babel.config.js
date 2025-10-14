da0ed6dc4dc1e01bf2cd28e831e56fec1e3b0087
l-gustavo-alves <felps4714@gmail.com>
2025-10-13 21:47:20 -0300

    App crashing when start fix

--- babel.config.js
+++ babel.config.js
@@ -4,9 +4,6 @@ module.exports = function (api) {
       presets: [
         ["babel-preset-expo", { jsxImportSource: "nativewind" }],
         "nativewind/babel",
-        "@babel/preset-typescript",
-        "@babel/preset-react",
       ],
-      plugins: ["@babel/plugin-transform-runtime"],
     };
   };
