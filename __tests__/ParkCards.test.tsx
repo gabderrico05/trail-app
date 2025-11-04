917bed3f9d6255b2bb1f078b088669a064df3af5
l-gustavo-alves <felps4714@gmail.com>
2025-11-03 21:36:15 -0300

    Fix and implementation of TrailCard and ParkCards tests

--- __tests__/ParkCards.test.tsx
+++ __tests__/ParkCards.test.tsx
@@ -1,8 +1,29 @@
-import { render } from '@testing-library/react-native';
+import { fireEvent, render } from '@testing-library/react-native';
+import { router } from 'expo-router';
 import React from 'react';
 import ParkCard from '../components/ParkCards';
+
+jest.mock('expo-router', () => ({ router: { push: jest.fn() } }));
+
 
 test('ParkCard renders correctly', () => {
     render(<ParkCard title="Yellowstone" subtitle="First national park in the U.S." />);
 
 });
+
+test('ParkCard is clickable and triggers navigation via onPress', () => {
+  const navigation = { navigate: jest.fn() };
+  const onPress = () => router.push('/selectTrail');
+
+  const { getByText } = render(
+    <ParkCard
+      title="Yellowstone"
+      subtitle="First national park in the U.S."
+      onPress={onPress}
+    />
+  );
+
+  fireEvent.press(getByText('Yellowstone'));
+  expect(router.push).toHaveBeenCalledWith('/selectTrail');
+});
+
--- jest.config.js
+++ jest.config.js
@@ -5,10 +5,13 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 /** @type {import("jest").Config} **/
 module.exports = {
   testEnvironment: "node",
-  preset: 'react-native',
+  preset: 'jest-expo',
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
+  transformIgnorePatterns: [
+      "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg)"
+  ],
   moduleNameMapper: {
     '\\.svg$': '<rootDir>/__mocks__/svgMock.js',
   },
