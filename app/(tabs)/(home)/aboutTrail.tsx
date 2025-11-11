1896f4c886a45c6dc7d0223345f5439c25ab8f16
MURILO FRANCISCO MATOS <CG3028151@car.ifsp>
2025-11-10 22:17:53 -0300

    Feat: webview at about trail page and finish integration with list entities

--- app/(tabs)/(home)/aboutTrail.tsx
+++ app/(tabs)/(home)/aboutTrail.tsx
@@ -12,11 +12,18 @@ function aboutTrail() {
         <Foundation name="info" size={24} color="#113D31" />
         <Text className="font-bold text-forestGreen-500">Sobre a Trilha</Text>
       </View>
-      <View className="flex-1">
+      <View className="flex-1 p-6 bg-white">
         <WebView
           originWhitelist={["*"]}
           style={{ background: "black" }}
-          source={{ html: "<h1>oi</h1>" }}
+          source={{ html: `
+            <h1 style='color: #113D31; font-size: 4rem'>Lorem ipsum</h1>
+            <p style='font-size: 3rem'>Lorem ipsum dolor sit amet consectetur. Sed aliquet enim elit massa et morbi massa lorem. Sed arcu egestas non condimentum. Mattis cras maecenas enim tristique egestas morbi. Scelerisque ac et consectetur amet at molestie tortor at.</p>
+            <h1 style='color: #113D31; font-size: 4rem'>Lorem ipsum</h1>
+            <p style='font-size: 3rem'>Lorem ipsum dolor sit amet consectetur. Sed aliquet enim elit massa et morbi massa lorem. Sed arcu egestas non condimentum. Mattis cras maecenas enim tristique egestas morbi. Scelerisque ac et consectetur amet at molestie tortor at.</p>
+            <h1 style='color: #113D31; font-size: 4rem'>Lorem ipsum</h1>
+            <p style='font-size: 3rem'>Lorem ipsum dolor sit amet consectetur. Sed aliquet enim elit massa et morbi massa lorem. Sed arcu egestas non condimentum. Mattis cras maecenas enim tristique egestas morbi. Scelerisque ac et consectetur amet at molestie tortor at.</p>s
+            ` }}
         />
       </View>
 
--- components/TrailHeader.tsx
+++ components/TrailHeader.tsx
@@ -5,23 +5,24 @@ import ReturnButton from "./ReturnButton";
 function TrailHeader() {
   return (
     <ImageBackground
-      className="p-6"
       style={{ height: "auto", width: "100%" }}
       source={require("@/assets/imageTrilha.jpg")}
       resizeMode="cover"
     >
-      <View className="w-full ">
-        <ReturnButton buttonType="secondary" />
-      </View>
-      <View className="bg-white items-center p-2 px-4 mt-4 rounded-2xl flex-row">
-        <Text className="text-forestGreen-500 font-bold mr-auto">
-          Trilha da Pedra Branca
-        </Text>
-        <Image
-          source={require("@/assets/parqueEstadual.png")}
-          style={{ width: 50, height: 50 }}
-          resizeMode="contain"
-        />
+      <View className="p-6">
+        <View className="w-full">
+          <ReturnButton buttonType="secondary" />
+        </View>
+        <View className="bg-white items-center p-2 px-4 mt-4 rounded-2xl flex-row">
+          <Text className="text-forestGreen-500 font-bold mr-auto">
+            Trilha da Pedra Branca
+          </Text>
+          <Image
+            source={require("@/assets/parqueEstadual.png")}
+            style={{ width: 50, height: 50 }}
+            resizeMode="contain"
+            />
+        </View>
       </View>
     </ImageBackground>
   );
